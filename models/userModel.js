a44c631da17a8ff54c7f7bbb0d447611c82ef53a
Ranga-Meghana <m.r.meghana13065@gmail.com>
2025-06-03 13:02:57 +0530

    updated

---
+++ models/userModel.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema({
+    name: {type: String},
+    email: {type: String},
+    pass: {type: String},
+});
+const user = mongoose.model("User", userSchema);
