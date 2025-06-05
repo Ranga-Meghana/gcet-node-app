5db03ade5fbaed3a5d2cf3d15afbd01acb81e9c3
Ranga-Meghana <m.r.meghana13065@gmail.com>
2025-06-05 05:40:06 +0530

    Fixed orderRouter to use Mongoose properly

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,12 +3,19 @@ import orderModel from '../models/orderModel.js'
 
 const orderRouter = express.Router()
 
-orderRouter.post("/new", async(req, res)=>{
-    const {email,price} = req.body
+orderRouter.post("/new", async (req, res) => {
+  try {
+    const { email, price } = req.body;
+
     const newOrder = new orderModel({ email, price });
-    const result = await newOrder.save();
-    return res.json(result);
-})
+    const savedOrder = await newOrder.save();
+
+    return res.json(savedOrder);
+  } catch (err) {
+    return res.status(500).json({ message: "Error creating order", error: err.message });
+  }
+});
+
 
 orderRouter.get("/:id", async(req, res)=>{
     const email = req.params.id
