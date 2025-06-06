6e798f5daaeaf771080fa90eaae160a6bf1ce412
Ranga-Meghana <m.r.meghana13065@gmail.com>
2025-06-06 09:36:58 +0530

    updated

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+app.use(cors()); 
 app.use(express.json());
-app.use(cors());
+
+
+
+app.use("/orders", orderRouter); 
+
+app.use("/users", userRouter);
+
+app.use("/products", productRouter);
+
 
 app.get("/", (req, res) => {
-  return res.json(users);
+  return res.send("Hello! Good morning :)");
+});
+
+app.get("/weather", (req, res) => {
+  return res.send("Heyy!! Today's weather is 28"); 
+});
+
+app.get("/name", (req, res) => {
+  return res.send("Hello Rishitha!"); 
 });
 
 
-app.get("/products", (req, res) => {
-  const products = [
-    { id: 1, name: "Laptop", price: 999 },
-    { id: 2, name: "Phone", price: 499 },
-    { id: 3, name: "Headphones", price: 199 }
-  ];
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
   
-  return res.json(products);
-});
-
-app.listen(8080, () => {
-  console.log("Server Started on port 8080");
-});
-
-
-
-// import express from "express";
-// import cors from "cors";
-
-// const app = express();
-// app.use(cors()); 
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   return res.send("Hello");
-// });
-
-// app.get("/weather", (req, res) => {
-//   return res.send("28"); 
-// });
-
-// app.listen(8080, () => {
-//   console.log("Server Started on port 8080");
-// });
-
-
-// app.get("/home", (req, res) => {
-//   return res.send(""); 
-// });
